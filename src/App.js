e5ffb3c1ea17d367186b010b65602c8f06018788
MalvinL645 <alvinfadil11@gmail.com>
2020-10-16 21:00:26 +0700

    init routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-
 import './App.css';
+import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
+import Header from './header';
+import routes from './config/routes';
 
-function App() {
+const isUserAuthenticated = false;
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
   return (
-    <div className="App">
-      <h1>My First Heading</h1>
-      <p>My first paragraph.</p>
-    </div>
+    <Route
+      {...rest}
+      render={() => {
+        if (isUserAuthenticated) {
+          return <Component />;
+        }
+        return <Redirect to="/login" />;
+      }}
+    />
   );
-}
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        {routes.map((route) => {
+          if (route.isPublic) {
+            return (
+              <Route
+                path={route.path}
+                component={route.component}
+                key={route.path}
+              />
+            );
+          }
+          return (
+            <PrivateRoute
+              path={route.path}
+              component={route.component}
+              key={route.path}
+            />
+          );
+        })}
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
