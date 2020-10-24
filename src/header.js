0b5517846eb06c4f5637ebc63cfadb6d64782761
MalvinL645 <alvinfadil11@gmail.com>
2020-10-24 08:12:17 +0700

    custom Hooks

--- src/header.js
+++ src/header.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+
+const useTitle = () => {
+  const [idx, setIdx] = useState(0);
+  useEffect(() => {
+    const listMenu = ['home', 'about', 'skills', 'experience'];
+    document.title = `Alvin! - ${listMenu[idx]}`;
+  }, [idx]);
+
+  return [idx, setIdx];
+};
 
 const Header = () => {
   const listMenu = ['home', 'about', 'skills', 'experience'];
+  const [idx, setIdx] = useTitle();
   return (
     <section className="container navbar">
       <header>
@@ -11,10 +22,15 @@ const Header = () => {
         </div>
         <nav>
           <ul>
-            {listMenu.map((name) => {
+            {listMenu.map((name, index) => {
               return (
-                <li key={name}>
-                  <Link to={`/${name}`}>
+                <li key={idx + name}>
+                  <Link
+                    to={`/${name}`}
+                    onClick={() => {
+                      return setIdx(index);
+                    }}
+                  >
                     <div className="navbar__menu">{name}</div>
                   </Link>
                 </li>
